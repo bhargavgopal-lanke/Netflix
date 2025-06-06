6a24ac0a61ea2cd69d5ec2b597543ac09e662a5c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-06 10:32:08 -0400

    Refactor application structure by implementing routing in Body component, adding Browse and Login components, and integrating NetflixIcon. Update App.css for styling and include background image.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Body from "./components/Body";
-import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Body />
     </div>
   );
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Browse from "./Browse";
 
 const Body = () => {
   return (
-    <div>
-      Body
+    <div className="banner-img">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
