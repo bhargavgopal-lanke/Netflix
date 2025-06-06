3f19b5c2ab4bae16611e32dbab48f6bac73465cc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-05 20:16:54 -0400

    Implement routing with React Router and add Body component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Body from "./components/Body";
 import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
