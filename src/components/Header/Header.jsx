73200821f9fbeee161789e66e5846c2b49fa773b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-11 16:37:52 -0400

    Refactor Header component layout and styling; add user logo image and update CSS for better structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NetflixIcon from "../Icons/NetflixIcon";
+import userlogo from "../../Images/userlogoimage.jpg";
 
 const Header = () => {
   return (
-    <div className="netflix-header">
-      <NetflixIcon />
+    <div className="header-container">
+      <div className="netflix-header">
+        <NetflixIcon />
+      </div>
+      <div className="user-logo">
+        <img src={userlogo} alt="user-image" className="user-logo-img" />
+        <button type="button">Logout</button>
+      </div>
     </div>
   );
 };
