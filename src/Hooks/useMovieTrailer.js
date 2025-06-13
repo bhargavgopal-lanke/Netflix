09e0c23a31eff20c4d83794628f6aac08289a6f1
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-13 13:36:29 -0400

    Add useMovieTrailer hook to fetch and manage movie trailer data; refactor VedioTitle component to utilize the new hook

---
+++ src/Hooks/useMovieTrailer.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/utils";
+import { addTrailerMovie } from "../utils/Slices/NewMoviesList";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!movieId) return;
+    const fetchMoviesImageUrl = async () => {
+      const fetchData = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+        API_OPTIONS
+      );
+      const data = await fetchData.json();
+      const results = await data?.results;
+      const filterTrailer =
+        results &&
+        results.filter((filterMovie) => filterMovie.type === "Trailer");
+      const key = filterTrailer[0];
+      dispatch(addTrailerMovie(key));
+    };
+    fetchMoviesImageUrl();
+  }, [movieId, dispatch]);
+};
+
+export default useMovieTrailer;
