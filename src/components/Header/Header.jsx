ef8686c2c74370da66ba4f726b9b2b1712a6d95a
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-13 16:53:30 -0400

    Refactor Header component: improve scroll handling logic and cleanup event listener management

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,23 +6,39 @@ import { HandleSignout, handleSubscribe } from "../../utils/utils";
 
 const Header = () => {
   const [visible, setVisible] = useState(true);
-  let scrollTop = 0;
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
   useEffect(() => {
+    let lastScrollTop = 0;
     const unsubscribe = handleSubscribe(dispatch, navigate);
+
+    const handleScroll = () => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setVisible(scrollTop < lastScrollTop);
+      lastScrollTop = scrollTop;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
     // unsubscribe when the component unmounts
-    return () => unsubscribe();
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      unsubscribe();
+    };
   }, [dispatch, navigate]);
 
   const store = useSelector((state) => state?.userReducer?.user);
   const { displayName, photoURL } = store || "";
 
   return (
-    <div className="header-container">
+    <div
+      className="header-container"
+      style={{
+        top: visible ? "0" : "-100px",
+        transistion: "top 0.3s ease-in-out",
+      }}
+    >
       <div className="netflix-header">
         <NetflixIcon />
       </div>
@@ -30,7 +46,10 @@ const Header = () => {
         <div className="user-logo">
           <h4>{displayName}</h4>
           <img src={photoURL} alt="user-image" className="user-logo-img" />
-          <button type="button" onClick={() => HandleSignout(dispatch, navigate)}>
+          <button
+            type="button"
+            onClick={() => HandleSignout(dispatch, navigate)}
+          >
             Logout
           </button>
         </div>
