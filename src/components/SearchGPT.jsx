29b7396ecc40461b726f025cea70dd6217187a86
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-14 14:39:24 -0400

    Add SearchGPT component with styling for search implementation form

--- src/components/SearchGPT.jsx
+++ src/components/SearchGPT.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const SearchGPT = () => {
   return (
-    <div>
-      SearchGPT
+    <div className="searchgpt-banner">
+      <div className="search-implementation-form">
+        <form>
+          <input type="text" placeholder="search implementation" />
+          <button type="button">Search</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchGPT
+export default SearchGPT;
